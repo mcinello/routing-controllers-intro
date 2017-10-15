98e21452daafda4c6f592c78a374e53c35dc571a
Michelle <mcinello@Nikolas-iMac.local>
2017-10-15 18:19:10 -0400

    created route and controller method to view a kitten in their unique page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -12,4 +12,9 @@ class PagesController < ApplicationController
 		@header = "Enter Our Contest to Win a Prize!"	
 	end
 
+	def kitten
+		requested_size = params[:size]
+		@kitten_url = "http://lorempixel.com/#{requested_size}/#{requested_size}/cats"
+	end
+
 end
